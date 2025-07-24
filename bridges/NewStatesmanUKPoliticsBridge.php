07ee6d5a686612fa94e263ba4054b3df7a023c07
lburkie <liam@ltburke.com>
2025-07-24 21:50:00 +0100

    updated collectdata function for test bridges

--- bridges/NewStatesmanUKPoliticsBridge.php
+++ bridges/NewStatesmanUKPoliticsBridge.php
@@ -10,11 +10,42 @@ class NewStatesmanUKPoliticsBridge extends BridgeAbstract
     const CACHE_TIMEOUT = 3600; // Can be omitted!
     
     public function collectData()
-    {
-        $item = []; // Create an empty item
-    
-        $item['title'] = 'Hello World!';
-    
-        $this->items[] = $item; // Add item to the list
+{
+    $html = getSimpleHTMLDOM(self::URI) or returnServerError('Could not load New Statesman UK Politics page');
+
+    foreach ($html->find('article') as $article) {
+        $item = [];
+
+        // Link + Title
+        $linkElement = $article->find('a', 0);
+        if (!$linkElement) {
+            continue; // skip malformed articles
+        }
+        $item['uri'] = urljoin(self::URI, $linkElement->href);
+        $item['title'] = trim($linkElement->plaintext);
+
+        // Summary (optional)
+        $summary = $article->find('p', 0);
+        if ($summary) {
+            $item['content'] = trim($summary->plaintext);
+        }
+
+        // Date (optional)
+        $time = $article->find('time', 0);
+        if ($time && isset($time->datetime)) {
+            $item['timestamp'] = strtotime($time->datetime);
+        }
+
+        // Image (optional)
+        $img = $article->find('img', 0);
+        if ($img && isset($img->src)) {
+            $imgSrc = urljoin(self::URI, $img->src);
+            $item['enclosures'] = [$imgSrc]; // adds as media enclosure
+            $item['content'] .= '<br><img src="' . $imgSrc . '" />';
+        }
+
+        $this->items[] = $item;
     }
+}
+
 }
